010d383d01172e43057e34f18b45303b79254e87
Aditya mishra <34139097+aditya985@users.noreply.github.com>
2019-05-01 15:31:43 +0530

    Update run_classifier.py

--- run_classifier.py
+++ run_classifier.py
@@ -425,19 +425,16 @@ def convert_single_example(ex_index, example, label_list, ids,max_seq_length,
   # the entire model is fine-tuned.
   tokens = []
   segment_ids = []
-  tokens.append("[CLS]")
   segment_ids.append(0)
   for token in tokens_a:
     tokens.append(token)
     segment_ids.append(0)
-  tokens.append("[SEP]")
   segment_ids.append(0)
 
   if tokens_b:
     for token in tokens_b:
       tokens.append(token)
       segment_ids.append(1)
-    tokens.append("[SEP]")
     segment_ids.append(1)
 
   input_ids = tokenizer.convert_tokens_to_ids(tokens)
